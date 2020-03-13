2317af45b62c6d93bd5125483fb8377db3089568
probopastor <43503592+probopastor@users.noreply.github.com>
2020-03-13 05:35:12 -0500

    I made a temporary thing in order to upload this to Simmer.io
    Button that plays particle effects and a cool sound clip so we have "something"

---
+++ CIS452 - Final Project/Assets/Temp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip auroraBorealis;
+    public ParticleSystem buttonParticles;
+    GameObject buttonParticlesObj;
+    public float yParticleModifier = 0f;
+
+    public void ButtonParticles()
+    {
+        AudioSource.clip = auroraBorealis;
+        AudioSource.Play();
+
+        Vector3 doughnutParticleLocation = new Vector3(0, 0, 1);
+        buttonParticlesObj = Instantiate(buttonParticles.gameObject, doughnutParticleLocation, Quaternion.Euler(-90f, 0f, 0f));
+        buttonParticles.Play();
+
+        Destroy(buttonParticlesObj, 20f);
+    }
+}
