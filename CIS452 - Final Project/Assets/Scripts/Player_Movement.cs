e6a3a30c0c9985f94f9329d8090571db2464ff10
Connor <46971709+cmwolf19@users.noreply.github.com>
2020-03-31 17:25:49 -0500

    Art & Basic Player

---
+++ CIS452 - Final Project/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * 
+ * 
+ * 
+ * 
+ */
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    #region Publics Variables
+    public float moveSpeed;
+    public float jumpHeight;
+    public LayerMask mask;
+    #endregion
+
+    #region Private Variables
+    private Rigidbody2D rb;
+    private Vector3 settingVelocity = Vector3.zero;
+    private Animator anim;
+    #endregion
+
+    #region Unity Callbacks
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HorizontalCalculation();
+        JumpingCalculation();
+
+        rb.velocity = new Vector2(settingVelocity.x, settingVelocity.y + rb.velocity.y);
+
+        AnimationUpdate();
+    }
+    #endregion
+
+    #region Player_Movement Functions
+    void HorizontalCalculation()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+
+        settingVelocity = Vector3.right * horInput * moveSpeed * Time.deltaTime;
+    }
+
+    void JumpingCalculation()
+    {
+        bool canJump = Physics2D.Raycast(transform.position, Vector2.down, 1f, mask);
+        if (canJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                settingVelocity += Vector3.up * jumpHeight;
+            }
+        }
+
+        if (rb.velocity.y > 0)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                rb.gravityScale = 0.5f;
+            } else rb.gravityScale = 1f;
+        } else rb.gravityScale = 1;
+        
+    }
+    #endregion
+
+    #region Animation Functions
+    void AnimationUpdate()
+    {
+        SetDirection();
+        SetMoving();
+        SetAirborne();
+    }
+
+    void SetDirection()
+    {
+        if (rb.velocity.x != 0)
+            transform.localScale = new Vector3(Mathf.Sign(rb.velocity.x), 1, 1);
+    }
+
+    void SetMoving()
+    {
+        anim.SetBool("isMoving", Mathf.Abs(rb.velocity.x) > 0);
+    }
+
+    void SetAirborne()
+    {
+        anim.SetBool("isAirborne", !Physics2D.Raycast(transform.position, Vector2.down, 1f, mask));
+    }
+    #endregion
+}
