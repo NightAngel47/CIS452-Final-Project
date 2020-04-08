cc66f8629a21bf3949df924d5a767126f8200810
Steven <NightAngel47@users.noreply.github.com>
2020-04-08 18:56:29 -0500

    Simple Enemy Factory

---
+++ CIS452 - Final Project/Assets/Scripts/Simple Factory/EnemyFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simple_Factory
+{
+    public class EnemyFactory : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> enemies = new List<GameObject>();
+        public enum EnemyTypes
+        {
+            Bat,
+            Rat,
+            Archer,
+            Warrior,
+            Wizard
+        }
+            
+        public GameObject EnemyToSpawn(EnemyTypes enemyType)
+        {
+            GameObject enemyToSpawn = null;
+
+            switch (enemyType)
+            {
+                case EnemyTypes.Bat: enemyToSpawn = enemies[(int) EnemyTypes.Bat];
+                    break;
+                case EnemyTypes.Rat: enemyToSpawn = enemies[(int) EnemyTypes.Rat];
+                    break;
+                case EnemyTypes.Archer: enemyToSpawn = enemies[(int) EnemyTypes.Archer];
+                    break;
+                case EnemyTypes.Warrior: enemyToSpawn = enemies[(int) EnemyTypes.Warrior];
+                    break;
+                case EnemyTypes.Wizard: enemyToSpawn = enemies[(int) EnemyTypes.Rat];
+                    break;
+                default:
+                    Debug.LogError("Enemy type not listed. Passed type: " + enemyType);
+                    break;
+            }
+
+            return enemyToSpawn;
+        }
+    }
+}
---
+++ CIS452 - Final Project/Assets/Scripts/Simple Factory/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Simple_Factory
+{
+    [RequireComponent(typeof(EnemyFactory))]
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The type of enemy to spawn.")] private EnemyFactory.EnemyTypes enemyType;
+        [SerializeField, Tooltip("The minimum spawn rate for random spawn time."), Range(0.1f, 10f)] private float minSpawnRate = 1;
+        [SerializeField, Tooltip("The maximum spawn rate for random spawn time."), Range(0.1f, 10f)] private float maxSpawnRate = 2;
+        
+        private EnemyFactory enemyFactory;
+        private Vector3 spawnPos;
+
+        private void Awake()
+        {
+            enemyFactory = GetComponent<EnemyFactory>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SpawnEnemy());
+        }
+
+        private IEnumerator SpawnEnemy()
+        {
+            spawnPos = transform.position;
+            Instantiate(enemyFactory.EnemyToSpawn(enemyType), spawnPos, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(minSpawnRate, maxSpawnRate));
+            StartCoroutine(SpawnEnemy());
+        }
+    }
+}
