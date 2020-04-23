c05840905722accbff8fd26c43b8b068cc63a9e7
skyrimsanta <skyrimsanta@users.noreply.github.com>
2020-04-23 18:33:22 -0500

    Gates

---
+++ CIS452 - Final Project/Assets/Scripts/Gate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gate : MonoBehaviour
+{
+    public string nextLevel;
+    private bool canGo = false;
+    public GameObject textBox;
+
+    private void Start()
+    {
+        textBox.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canGo = true;
+            textBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canGo = false;
+            textBox.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) & canGo)
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
