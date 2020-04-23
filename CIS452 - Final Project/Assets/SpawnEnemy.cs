296b2463c5e8bd78d532b2318a7073039535c6e6
Steven <NightAngel47@users.noreply.github.com>
2020-04-23 18:07:06 -0500

    Updated Spawner

---
+++ CIS452 - Final Project/Assets/SpawnEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public string enemyType;
+    private float timer;
+    [SerializeField] private float spawnTime = 2f;
+    private bool inRadius;
+
+    private void Update()
+    {
+        if (inRadius)
+        {
+            timer += Time.deltaTime;
+            if (timer > spawnTime)
+            {
+                ObjectPooler.instance.SpawnFromPool(enemyType, transform.position, Quaternion.identity);
+                timer = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            inRadius = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            inRadius = false;
+        }
+    }
+}
